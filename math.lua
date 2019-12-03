c423ba2709b2be9238a283c5d5dd7d6d6d33c672
nazarpunk <nazarpunk@ya.ru>
2019-12-03 03:33:43 +0200

    math.lua

--- math.lua
+++ math.lua
@@ -2,7 +2,7 @@
 ---@param distance real
 ---@param angle real radian
 ---@return real
-math.offsetX       = function(x, distance, angle)
+function math.offsetX(x, distance, angle)
 	return distance * math.cos(angle) + x
 end
 
@@ -10,7 +10,7 @@ end
 ---@param distance real
 ---@param angle real radian
 ---@return real
-math.offsetY       = function(y, distance, angle)
+function math.offsetY(y, distance, angle)
 	return distance * math.sin(angle) + y
 end
 
@@ -19,7 +19,7 @@ end
 ---@param distance real
 ---@param angle real radian
 ---@return real, real
-math.offsetXY      = function(x, y, distance, angle)
+function math.offsetXY(x, y, distance, angle)
 	return distance * math.cos(angle) + x, distance * math.sin(angle) + y
 end
 
@@ -29,7 +29,7 @@ end
 ---@param d real общее расстояние до цели
 ---@param x real расстояние от исходной цели до точки
 ---@return real
-math.parabolaZ     = function(zs, ze, h, d, x)
+function math.parabolaZ(zs, ze, h, d, x)
 	return (2 * (zs + ze - 2 * h) * (x / d - 1) + ze - zs) * (x / d) + zs
 end
 
@@ -38,7 +38,7 @@ end
 ---@param xb real
 ---@param yb real
 ---@return real
-math.distanceXY    = function(xa, ya, xb, yb)
+function math.distanceXY(xa, ya, xb, yb)
 	local dx, dy = xb - xa, yb - ya
 	return math.sqrt(dx * dx + dy * dy)
 end
@@ -50,7 +50,7 @@ end
 ---@param yb real
 ---@param zb real
 ---@return real
-math.distanceXYZ   = function(xa, ya, za, xb, yb, zb)
+function math.distanceXYZ(xa, ya, za, xb, yb, zb)
 	local dx, dy, dz = xb - xa, yb - ya, zb - za
 	return math.sqrt(dx * dx + dy * dy + dz * dz)
 end
@@ -60,14 +60,14 @@ end
 ---@param xb real
 ---@param yb real
 ---@return real radian
-math.angleXY       = function(xa, ya, xb, yb)
+function math.angleXY(xa, ya, xb, yb)
 	return math.atan(yb - ya, xb - xa)
 end
 
 ---@param a real radian
 ---@param b real radian
 ---@return real radian
-math.angleDiff     = function(a, b)
+function math.angleDiff(a, b)
 	local c = a > b and a - b or b - a
 	local d = a > b and b - a + 2 * math.pi or a - b + 2 * math.pi
 	return c > d and d or c
@@ -81,7 +81,7 @@ end
 ---@param cy table
 ---@param r table
 ---@return boolean
-math.inCircleXY    = function(x, y, cx, cy, r)
+function math.inCircleXY(x, y, cx, cy, r)
 	return (cx - x) * (cx - x) + (cy - y) * (cy - y) < r * r
 end
 
@@ -89,7 +89,7 @@ end
 ---@param a real degrees
 ---@param b real degrees
 ---@return real degrees
-math.angleDiffDeg  = function(a, b)
+function math.angleDiffDeg(a, b)
 	a, b = math.abs(a), math.abs(b)
 	if a > b then
 		a, b = b, a
@@ -110,6 +110,6 @@ end
 ---@param xc real
 ---@param yc real
 ---@return real
-math.perpendicular = function(xa, ya, xb, yb, xc, yc)
+function math.perpendicular(xa, ya, xb, yb, xc, yc)
 	return math.sqrt((xa - xc) * (xa - xc) + (ya - yc) * (ya - yc)) * math.sin(math.atan(yc - ya, xc - xa) - math.atan(yb - ya, xb - xa))
 end
